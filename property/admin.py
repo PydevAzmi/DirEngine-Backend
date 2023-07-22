6b9bd19ef82d155c034e64310462d746412cef86
pydevazmi <pydevazmi@gmail.com>
2023-07-22 13:37:39 +0300

    Register Admin Site

--- property/admin.py
+++ property/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Book, Review, Category, Place, PropertyImages, Property
 # Register your models here.
+admin.site.register(Property)
+admin.site.register(PropertyImages)
+admin.site.register(Book)
+admin.site.register(Review)
+admin.site.register(Category)
+admin.site.register(Place)
