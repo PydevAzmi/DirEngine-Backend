114cfd1de0f97d9dfcfc633521d069769b07b04d
pydevazmi <pydevazmi@gmail.com>
2023-07-29 15:54:42 +0300

    Sending e-mail

--- project/settings.py
+++ project/settings.py
@@ -146,3 +146,12 @@ MEDIA_ROOT  = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# E-mail Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'pydevazmi@gmail.com'
+EMAIL_HOST_PASSWORD = '****************'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
